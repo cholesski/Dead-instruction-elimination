86dbed84b9553b74535a5a49cd81acf82fb6c20f
user <user@irvm.localdomain>
2023-09-24 16:01:07 +0200

    Valjda ce sad da radi

---
+++ Tests/4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int compute(){
+    return 42;
+}
+
+int main()
+{
+    int x = 5;
+    x = compute();
+
+    return 0;
+}
---
+++ Tests/5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(){
+    int x = 5;
+    int *ptr = &x;
+    *ptr = 10;
+    return 0;
+}
